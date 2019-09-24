17ca1078bf18be659783b14f980429211991b224
oscarc04 <55443597+oscarc04@users.noreply.github.com>
2019-09-24 08:39:29 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,22 @@
+// Name: Oscar Carcamo
+// Intro to computer science
+// Mr.Williams
+// Period 1
+// 2019-09-24-classwork
+
+#include <iostream>
+
+int main()
+{
+	int onjah = 7;
+	int h = 9;
+	int w = 3;
+	int s = 8;
+	int a = 4;
+	int oc = 6;
+
+
+
+		std::cout << h << std::endl;
+
+}
