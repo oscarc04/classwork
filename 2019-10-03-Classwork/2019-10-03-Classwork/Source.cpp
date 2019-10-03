58886529c4ed7921f5ab50fc6d69eb2de7f1b368
oscarc04 <55443597+oscarc04@users.noreply.github.com>
2019-10-03 08:39:42 -0400

    Add files via upload

---
+++ 2019-10-03-Classwork/2019-10-03-Classwork/Source.cpp
@@ -0,0 +1,15 @@
+// Oscar Carcamo
+// Intro to CS 
+// Period 1
+// Mr.Williams
+// 2019-10-03-Classwork
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	double result = (9.5 * 4.5) - (2.5 * 3) / (45.5 - 3.5);
+	cout << result << endl;
+
+}
